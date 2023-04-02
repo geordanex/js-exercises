96f5c5c8c94c0b3830ed7649c9e53e7e9faed098
Geordan Sanchez <geordan.sanchez.contractor@bbva.com>
2023-04-02 17:17:13 -0500

    finish number exercises

--- exercises/basics.js
+++ exercises/basics.js
@@ -6,12 +6,12 @@ function isCero(num) {
 }
 
 // Devuelve true si el número dado es positivo (mayor que 0) y falso en caso contrario.
-function isPositivo(num) {
+function isPositiveNumber(num) {
   // Aqui va tu codigo
 }
 
 // Devuelve verdadero si el número dado es negativo (menor que 0) y falso en caso contrario.
-function isNegativo(num) {
+function isNegativeNumber(num) {
   // Aqui va tu codigo
 }
 
@@ -27,7 +27,7 @@ function isNegativo(num) {
  * Pares: -10, -12, -104, 0, 16, 18, 2
  * Impares: -11, -13, -103, -1, 1, 17, 19, 3
  */
-function isPar(num) {
+function isEven(num) {
   // Aqui va tu codigo
 }
 
@@ -39,7 +39,7 @@ function isPar(num) {
  * Par: -10, -12, -104, 0, 16, 18, 2
  * Impar: -11, -13, -103, -1, 1, 17, 19, 3
  */
-function isImpar(num) {
+function isOdd(num) {
   // Aqui va tu codigo
 }
 
@@ -57,6 +57,7 @@ function isImpar(num) {
  *  remainderOf(129, 17); // => 10
  *
  */
+
 function remainderOf(num, d) {
   // Aqui va tu codigo
 }
@@ -76,52 +77,233 @@ function esMultiploDe(n, d) {
   // Aqui va tu codigo
 }
 
-function power(num) {
+/**
+ *
+ *    Dados una base y un exponente entero positivo, devuelve la base elevada a la potencia del exponente.
+ *
+ *    @example
+ *    power(2, 3) // => 8 (es decir, 2 * 2 * 2)
+ *    power(-2, 3) // => -4 (es decir, -2 * -2 * -2)
+ *    power(5, 6) // => 15625 (es decir, 5 * 5 * 5 * 5 * 5 * 6)
+ */
+function power(base, exp) {
   // Aqui va tu codigo
 }
 
-function isPowerOf(num) {
+/**
+ *
+ *    Dados dos enteros no negativos, "num" y "base", devuelve true si num es una potencia de base y false en caso contrario.
+ *    Un número es una potencia de "base" si se puede escribir como base**k para algún entero no negativo k, donde "**" representa el operador de exponenciación.
+ *
+ *    Por ejemplo, 8 es una potencia de 2 ya que 8 es igual a 2 * 2 * 2 === 2**3.
+ *    Por el contrario, 81 no es una potencia de 2 ya que no existe un entero k tal que 81 === 2**k.
+ *    Sin embargo, 81 sí es una potencia de 3 ya que 81 === 3**4.
+ *
+ *    @example
+ *    isPowerOf(2, 2) // => true
+ *    isPowerOf(3, 3) // => true
+ *    isPowerOf(10, 2) // => false
+ */
+function isPowerOf(num, base) {
   // Aqui va tu codigo
 }
 
+/**
+ *
+ *   Dado un número entero no negativo como entrada, devuelve true si es una potencia de 2
+ *   y false de lo contrario.
+ *
+ *   Un número es una potencia de 2 si puede ser escrito como 2**k para algún entero no negativo k,
+ *   donde "**" representa el operador de exponenciación.
+ *
+ *   Por ejemplo, 8 es una potencia de 2 porque 8 es igual a 2 * 2 * 2 === 2**3.
+ *   Por el contrario, 81 no es una potencia de 2 porque no existe un entero k tal que
+ *   81 === 2**k.
+ *
+ *    @example
+ *    isPowerOfTwo(2) // => true
+ *    isPowerOfTwo(3) // => false
+ *    isPowerOfTwo(4) // => true
+ *    isPowerOfTwo(2**20) // => true
+ *    isPowerOfTwo(2**20 + 1) // => false
+ */
 function isPowerofTwo(num) {
   // Aqui va tu codigo
 }
 
-function isCuadradoPerfecto(num) {
+/**
+ *
+ *    Determine si el número entero dado es un cuadrado perfecto.
+ *    Un entero n es un cuadrado perfecto si hay algún otro entero k tal que
+ *    k * k === n. Es decir, si la raíz cuadrada de n también es un entero.
+ *
+ *    Advertencia: Si utiliza Math.sqrt, obtendrá respuestas incorrectas. Trate de encontrar
+ *    otra manera.
+ *
+ *    @example
+ *    isPerfectSquare(-1); // => false (ningún número entero al cuadrado puede ser negativo)
+ *    isPerfectSquare(0); // => true
+ *    isPerfectSquare(1); // => true
+ *    isPerfectSquare(2); // => false
+ *    isPerfectSquare(9); // => true (ya que 3*3 es 9)
+ */
+function isPerfectSquare(num) {
   // Aqui va tu codigo
 }
 
+/**
+ *
+ *    Dado un entero positivo, devuelve una array (arreglo) que consiste en todos sus factores
+ *    ordenados de menor a mayor.
+ *
+ *    @example
+ *    factors(2); // => [1, 2]
+ *    factors(4); // => [1, 2, 4]
+ *    factors(12); // => [1, 2, 3, 4, 6, 12]
+ */
 function factors(num) {
   // Aqui va tu codigo
 }
 
-function isPrimo(num) {
+/**
+ *
+ *    Devuelve true si el número entero positivo dado es primo y false en caso contrario.
+ *    Un número entero positivo es primo si sus únicos factores son 1 y el mismo.
+ *    Ver https://es.wikipedia.org/wiki/N%C3%BAmero_primo
+ *
+ *    Primos: 2, 3, 5, 7, 11, 13, 17, ...
+ *
+ *    @param {num} n - El número entero positivo a verificar si es primo.
+ *    @returns {boolean} true si el número es primo, false en caso contrario.
+ *
+ *    @example
+ *    isPrime(2); // => true
+ *    isPrime(4); // => false
+ *    isPrime(11); // => true
+ *    isPrime(15); // => false
+ */
+function isPrime(n) {
   // Aqui va tu codigo
 }
 
+/**
+ *
+ *    Dado un entero positivo, devuelve un array con todos sus factores primos
+ *    ordenados de menor a mayor. Cada primo aparece en el array tantas veces como
+ *    veces divide al entero dado.
+ *
+ *    @example
+ *    primeFactors(2); // => [2]
+ *    primeFactors(2 * 2); // => [2, 2]
+ *    primeFactors(2 * 2 * 3); // => [2, 2, 3]
+ *    primeFactors(2 * 2 * 3 * 17 * 197); // => [2, 2, 3, 17, 197]
+ *    primeFactors(917329); // => [7, 7, 97, 193]
+ */
 function primeFactors(num) {
   // Aqui va tu codigo
 }
 
-function sumOfRange(num) {
+/**
+ *
+ *    Dados dos enteros, devuelve la suma de todos los enteros entre esos dos enteros, incluyéndolos.
+ *    Por ejemplo:
+ *
+ *    @example
+ *    sumOfRange(1, 5) // => 15 (es decir, 1 + 2 + 3 + 4 + 5)
+ *    sumOfRange(-3, 4) // => 4 (es decir, -3 + -2 + -1 + 0 + 1 + 2 + 3 + 4)
+ */
+function sumOfRange(n1, n2) {
   // Aqui va tu codigo
 }
 
+/**
+ *
+ *    Dado un entero no negativo, devuelve su factorial.
+ *    Si n es un entero no negativo, su factorial se define como
+ *    n * (n - 1) * (n - 2) * ... * 2 * 1.
+ *
+ *    Por ejemplo, el factorial
+ *    de 5 es 5 * 4 * 3 * 2 * 1 o 120.
+ *
+ *    En papel, escribimos `5!` para representar el factorial de 5, pero en el código
+ *    definiríamos una función como factorial(5).
+ *    Ver https://en.wikipedia.org/wiki/Factorial
+ *
+ *    La función factorial crece muy rápido.
+ *    @example
+ *    factorial(1); // => 1
+ *    factorial(2); // => 2
+ *    factorial(3); // => 6
+ *    factorial(5); // => 120
+ *    factorial(10); // => 3628800
+ */
 function factorial(num) {
   // Aqui va tu codigo
 }
 
-function celsiusToFahrenheit(num) {
+/**
+ * Dada una temperatura en grados Celsius, devuelve la temperatura en grados Fahrenheit.
+ *
+ * @param {number} temp - La temperatura en grados Celsius
+ * @returns {number} La temperatura en grados Fahrenheit
+ */
+function celsiusToFahrenheit(temp) {
   // Aqui va tu codigo
 }
 
-function fahrenheitToCelsius(num) {
+/**
+ * Dada una temperatura en grados Fahrenheit, devuelve la temperatura en grados Celsius.
+ *
+ * @param {number} temp - La temperatura en grados Fahrenheit
+ * @returns {number} La temperatura en grados Celsius
+ */
+function fahrenheitToCelsius(temp) {
   // Aqui va tu codigo
 }
 
-function fibonacci(num) {
+/**
+ *
+ *    Dado un número entero no negativo n, devuelve el n-ésimo número de Fibonacci.
+ *    Los números de Fibonacci se definen así:
+ *    fib(n): 0 1 1 2 3 5 8 13 21 34 55 ...
+ *        n: 0  1  2  3  4  5  6   7   8   9  10  ...
+ *
+ *  Es decir, comenzando con 0 y 1, el siguiente número de Fibonacci es la suma
+ *  de los dos anteriores. El "0-ésimo" número de Fibonacci es 0 y el primer
+ *  número de Fibonacci es 1.
+ *  Ver https://es.wikipedia.org/wiki/Sucesi%C3%B3n_de_Fibonacci
+ *
+ *  @example
+ *  fibonacci(0); // => 0
+ *  fibonacci(1); // => 1
+ *  fibonacci(10); // => 55
+ *  fibonacci(12); // => 144
+ *
+ *  @param {number} n - Un número entero no negativo
+ *  @returns {number} El n-ésimo número de Fibonacci
+ */
+function fibonacci(n) {
   // Aqui va tu codigo
 }
 
-module.exports = isCero;
+module.exports = {
+  isCero,
+  isPositiveNumber,
+  isNegativeNumber,
+  isEven,
+  isOdd,
+  remainderOf,
+  esMultiploDe,
+  power,
+  isPowerOf,
+  isPowerofTwo,
+  isPerfectSquare,
+  factors,
+  isPrime,
+  primeFactors,
+  sumOfRange,
+  factorial,
+  celsiusToFahrenheit,
+  fahrenheitToCelsius,
+  fibonacci,
+};
--- tests/test.js
+++ tests/test.js
@@ -22,21 +22,21 @@ describe("isCero", () => {
   });
 });
 
-describe("positiveNumber", () => {
+describe("isPositiveNumber", () => {
   test("Debería devolver true para números positivos", () => {
-    expect(positiveNumber(3)).toBe(true);
-    expect(positiveNumber(0.1)).toBe(true);
-    expect(positiveNumber(1000)).toBe(true);
+    expect(isPositiveNumber(3)).toBe(true);
+    expect(isPositiveNumber(0.1)).toBe(true);
+    expect(isPositiveNumber(1000)).toBe(true);
   });
 
   test("Debería devolver false para números negativos", () => {
-    expect(positiveNumber(-1)).toBe(false);
-    expect(positiveNumber(-0.5)).toBe(false);
-    expect(positiveNumber(-1000)).toBe(false);
+    expect(isPositiveNumber(-1)).toBe(false);
+    expect(isPositiveNumber(-0.5)).toBe(false);
+    expect(isPositiveNumber(-1000)).toBe(false);
   });
 
   test("Debería devolver false para el número 0", () => {
-    expect(positiveNumber(0)).toBe(false);
+    expect(isPositiveNumber(0)).toBe(false);
   });
 });
 
@@ -98,23 +98,23 @@ describe("isEven", () => {
   });
 });
 
-describe("esImpar", () => {
+describe("isOdd", () => {
   test("retorna verdadero para numeros impares", () => {
-    expect(esImpar(1)).toBe(true);
-    expect(esImpar(3)).toBe(true);
-    expect(esImpar(-11)).toBe(true);
-    expect(esImpar(-7)).toBe(true);
+    expect(isOdd(1)).toBe(true);
+    expect(isOdd(3)).toBe(true);
+    expect(isOdd(-11)).toBe(true);
+    expect(isOdd(-7)).toBe(true);
   });
 
   test("retorna falso para numeros pares", () => {
-    expect(esImpar(2)).toBe(false);
-    expect(esImpar(4)).toBe(false);
-    expect(esImpar(-10)).toBe(false);
-    expect(esImpar(-8)).toBe(false);
+    expect(isOdd(2)).toBe(false);
+    expect(isOdd(4)).toBe(false);
+    expect(isOdd(-10)).toBe(false);
+    expect(isOdd(-8)).toBe(false);
   });
 
   test("retorna falso para el número 0", () => {
-    expect(esImpar(0)).toBe(false);
+    expect(isOdd(0)).toBe(false);
   });
 });
 
@@ -164,5 +164,243 @@ describe("esMultiploDe", () => {
     expect(esMultiploDe(5, 2)).toBe(false);
     expect(esMultiploDe(7, 3)).toBe(false);
     expect(esMultiploDe(101, 10)).toBe(false);
+  });
+});
+
+describe("power", () => {
+  test("devuelve la base elevada al exponente", () => {
+    expect(power(2, 3)).toEqual(8);
+    expect(power(-2, 3)).toEqual(-8);
+    expect(power(5, 6)).toEqual(15625);
+  });
+
+  test("lanza un error al elevar una base negativa a un exponente fraccionario", () => {
+    expect(() => power(-2, 0.5)).toThrowError("Exponente no entero");
+  });
+});
+
+describe("isPowerOf", () => {
+  test("debería devolver true cuando num es una potencia de base", () => {
+    expect(isPowerOf(8, 2)).toBe(true);
+    expect(isPowerOf(16, 2)).toBe(true);
+    expect(isPowerOf(27, 3)).toBe(true);
+    expect(isPowerOf(81, 3)).toBe(true);
+    expect(isPowerOf(625, 5)).toBe(true);
+    expect(isPowerOf(1, 1)).toBe(true);
+  });
+
+  test("debería devolver false cuando num no es una potencia de base", () => {
+    expect(isPowerOf(10, 2)).toBe(false);
+    expect(isPowerOf(15, 2)).toBe(false);
+    expect(isPowerOf(40, 3)).toBe(false);
+    expect(isPowerOf(100, 5)).toBe(false);
+    expect(isPowerOf(50, 7)).toBe(false);
+  });
+});
+
+describe("isPowerOfTwo", () => {
+  test("retorna verdadero para potencias de 2", () => {
+    expect(isPowerOfTwo(2)).toBe(true);
+    expect(isPowerOfTwo(4)).toBe(true);
+    expect(isPowerOfTwo(2 ** 10)).toBe(true);
+  });
+
+  test("retorna falso para números que no son potencias de 2", () => {
+    expect(isPowerOfTwo(3)).toBe(false);
+    expect(isPowerOfTwo(5)).toBe(false);
+    expect(isPowerOfTwo(2 ** 10 + 1)).toBe(false);
+  });
+});
+
+describe("isPerfectSquare", () => {
+  test("retorna true si el número es un cuadrado perfecto", () => {
+    expect(isPerfectSquare(0)).toBe(true);
+    expect(isPerfectSquare(1)).toBe(true);
+    expect(isPerfectSquare(4)).toBe(true);
+    expect(isPerfectSquare(9)).toBe(true);
+    expect(isPerfectSquare(16)).toBe(true);
+    expect(isPerfectSquare(25)).toBe(true);
+    expect(isPerfectSquare(36)).toBe(true);
+    expect(isPerfectSquare(49)).toBe(true);
+    expect(isPerfectSquare(64)).toBe(true);
+    expect(isPerfectSquare(81)).toBe(true);
+    expect(isPerfectSquare(100)).toBe(true);
+  });
+
+  test("retorna false si el número no es un cuadrado perfecto", () => {
+    expect(isPerfectSquare(2)).toBe(false);
+    expect(isPerfectSquare(3)).toBe(false);
+    expect(isPerfectSquare(5)).toBe(false);
+    expect(isPerfectSquare(7)).toBe(false);
+    expect(isPerfectSquare(10)).toBe(false);
+    expect(isPerfectSquare(15)).toBe(false);
+    expect(isPerfectSquare(20)).toBe(false);
+    expect(isPerfectSquare(30)).toBe(false);
+    expect(isPerfectSquare(50)).toBe(false);
+    expect(isPerfectSquare(99)).toBe(false);
+  });
+
+  test("retorna false si el número es negativo", () => {
+    expect(isPerfectSquare(-1)).toBe(false);
+    expect(isPerfectSquare(-2)).toBe(false);
+    expect(isPerfectSquare(-10)).toBe(false);
+  });
+});
+
+describe("factors", () => {
+  test("devuelve un array con los factores de un número en orden ascendente", () => {
+    expect(factors(1)).toEqual([1]);
+    expect(factors(2)).toEqual([1, 2]);
+    expect(factors(4)).toEqual([1, 2, 4]);
+    expect(factors(12)).toEqual([1, 2, 3, 4, 6, 12]);
+    expect(factors(15)).toEqual([1, 3, 5, 15]);
+  });
+
+  it("debería lanzar un error si el array resultante no está en orden ascendente", () => {
+    const result = factors(12);
+    for (let i = 0; i < result.length - 1; i++) {
+      expect(result[i]).toBeLessThan(result[i + 1]);
+    }
+  });
+});
+
+describe("isPrime", () => {
+  it("Debería devolver true si el número es primo", () => {
+    expect(isPrime(2)).toBe(true);
+    expect(isPrime(3)).toBe(true);
+    expect(isPrime(5)).toBe(true);
+    expect(isPrime(7)).toBe(true);
+    expect(isPrime(11)).toBe(true);
+    expect(isPrime(13)).toBe(true);
+    expect(isPrime(17)).toBe(true);
+  });
+
+  it("Debería devolver false si el número no es primo", () => {
+    expect(isPrime(4)).toBe(false);
+    expect(isPrime(6)).toBe(false);
+    expect(isPrime(9)).toBe(false);
+    expect(isPrime(15)).toBe(false);
+    expect(isPrime(21)).toBe(false);
+  });
+
+  it("Debería devolver false si el número es 1", () => {
+    expect(isPrime(1)).toBe(false);
+  });
+
+  it("Debería devolver false si el número es negativo", () => {
+    expect(isPrime(-2)).toBe(false);
+    expect(isPrime(-7)).toBe(false);
+  });
+
+  it("Debería devolver false si el número es cero", () => {
+    expect(isPrime(0)).toBe(false);
+  });
+});
+
+describe("primeFactors", () => {
+  test("devuelve un array con los factores primos del número", () => {
+    expect(primeFactors(2)).toEqual([2]);
+    expect(primeFactors(4)).toEqual([2, 2]);
+    expect(primeFactors(12)).toEqual([2, 2, 3]);
+    expect(primeFactors(34)).toEqual([2, 17]);
+    expect(primeFactors(917329)).toEqual([7, 7, 97, 193]);
+  });
+
+  test("devuelve un array vacío para números menores o iguales a 1", () => {
+    expect(primeFactors(0)).toEqual([]);
+    expect(primeFactors(1)).toEqual([]);
+    expect(primeFactors(-10)).toEqual([]);
+  });
+
+  test("devuelve un array vacío para números no enteros", () => {
+    expect(primeFactors(2.5)).toEqual([]);
+    expect(primeFactors(Math.PI)).toEqual([]);
+  });
+
+  test("devuelve un array vacío para números grandes", () => {
+    expect(primeFactors(10 ** 10)).toEqual([]);
+  });
+
+  test("devuelve factores primos en orden ascendente incluso si no están ordenados", () => {
+    expect(primeFactors(2 * 2 * 3 * 17 * 197)).toEqual([2, 2, 3, 17, 197]);
+    expect(primeFactors(3 * 7 * 11)).toEqual([3, 7, 11]);
+    expect(primeFactors(2 * 2 * 2 * 2 * 7)).toEqual([2, 2, 2, 2, 7]);
+    expect(primeFactors(17)).toEqual([17]);
+  });
+});
+
+describe("sumOfRange", () => {
+  test("retorna la suma de los enteros en el rango dado", () => {
+    expect(sumOfRange(1, 5)).toBe(15);
+    expect(sumOfRange(-3, 4)).toBe(4);
+    expect(sumOfRange(0, 0)).toBe(0);
+    expect(sumOfRange(-10, -5)).toBe(-45);
+  });
+
+  test("retorna 0 si los valores de inicio y final son iguales", () => {
+    expect(sumOfRange(2, 2)).toBe(2);
+    expect(sumOfRange(0, 0)).toBe(0);
+    expect(sumOfRange(-3, -3)).toBe(-3);
+  });
+
+  test("retorna NaN si alguno de los parámetros no es un número", () => {
+    expect(sumOfRange(2, "5")).toBe(NaN);
+    expect(sumOfRange(true, 5)).toBe(NaN);
+    expect(sumOfRange("hola", "mundo")).toBe(NaN);
+  });
+});
+
+describe("factorial", () => {
+  test("retorna el factorial de un número dado", () => {
+    expect(factorial(0)).toBe(1);
+    expect(factorial(1)).toBe(1);
+    expect(factorial(2)).toBe(2);
+    expect(factorial(5)).toBe(120);
+    expect(factorial(10)).toBe(3628800);
+  });
+
+  test("retorna un error si el número es negativo", () => {
+    expect(() => factorial(-1)).toThrow("El número debe ser no negativo");
+    expect(() => factorial(-10)).toThrow("El número debe ser no negativo");
+  });
+});
+
+describe("celsiusToFahrenheit", () => {
+  test("debería devolver la temperatura en Fahrenheit", () => {
+    expect(celsiusToFahrenheit(0)).toBe(32);
+    expect(celsiusToFahrenheit(10)).toBe(50);
+    expect(celsiusToFahrenheit(20)).toBe(68);
+    expect(celsiusToFahrenheit(30)).toBe(86);
+  });
+});
+
+describe("fahrenheitToCelsius", () => {
+  test("convierte 32 grados Fahrenheit a 0 grados Celsius", () => {
+    expect(fahrenheitToCelsius(32)).toBe(0);
+  });
+
+  test("convierte 68 grados Fahrenheit a 20 grados Celsius", () => {
+    expect(fahrenheitToCelsius(68)).toBe(20);
+  });
+
+  test("convierte -22 grados Fahrenheit a -30 grados Celsius", () => {
+    expect(fahrenheitToCelsius(-22)).toBe(-30);
+  });
+});
+
+describe("fibonacci", () => {
+  test("devuelve el n-ésimo número de Fibonacci", () => {
+    expect(fibonacci(0)).toBe(0);
+    expect(fibonacci(1)).toBe(1);
+    expect(fibonacci(5)).toBe(5);
+    expect(fibonacci(10)).toBe(55);
+  });
+
+  test("devuelve 0 para n = 0", () => {
+    expect(fibonacci(0)).toBe(0);
+  });
+
+  test("devuelve 1 para n = 1", () => {
+    expect(fibonacci(1)).toBe(1);
   });
 });
